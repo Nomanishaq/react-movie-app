93fc248579341bb52fbcff0c9be361c2efb3c1c4
nomanishaq <nomanishaq251@gmail.com>
2020-06-25 16:53:30 +0500

    update

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -3,29 +3,29 @@ import {MovieContext} from './MovieContext';
 
 
 const AddMovie=()=> {
-
-const updateName = e =>{
-    setName(e.target.value);
-}
-
-const updatePrice =  e =>{
-    setPrice(e.target.value);
-}
-
-const [name, setName]= useState();
-const [price,setPrice] = useState();
-const movie = useContext(MovieContext);
+    
+    let [name, setName]= useState();
+    let [price,setPrice] = useState();
+    let movie = useContext(MovieContext);
+    
+    const updateName = e =>{
+        setName(e.target.value);
+        
+    }
+    
+    const updatePrice =  e =>{
+        setPrice(e.target.value);
+    }
 
 const addmovie = e =>{
     e.preventDefault();
-    movie[1](prevMovies => [...prevMovies,{name: name, price: price,id: Math.random()}]);
-    setName('');
+    movie[1](prevMovies => [...prevMovies,{name: name, price: price,id: Math.random()}]);    
 }
 
     return (
         <div>
             <form onSubmit={addmovie}>
-                <input type="text" placeholder="Movie Name" onChange={updateName} name={name} required/><br/>
+                <input type="text" placeholder="Movie Name"  onChange={updateName} name={name} required/><br/>
                 <input type="number" placeholder="Price" onChange={updatePrice}  name={price} required/><br/><br/>
                 <input type="submit" value="Add"/>
             </form>
